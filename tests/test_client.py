a7ce393f671dcdb4d5b4826eadfbdc357107001a
Noah Woehler <noah.woehler@gmail.com>
2020-11-03 02:56:08 +0100

    Add certbot tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -26,7 +26,6 @@ class TestClient:
 
     def setUp(self) -> None:
         self.log = logging.getLogger(f"acme_broker.tests.{self.name}")
-        logging.getLogger("asyncio").setLevel(logging.INFO)
         self.contact = f"woehler+{self.name}@luis.uni-hannover.de"
         dir_ = Path("./tmp") / self.name
         if not dir_.exists():
@@ -60,8 +59,8 @@ class TestClient:
 
     async def asyncSetUp(self) -> None:
         self.loop = asyncio.get_event_loop()
-        config = load_config("../debug.yml")
-        runner, ca = await AcmeCA.runner(*config["ca"].values())
+        self.config = load_config("../debug.yml")
+        runner, ca = await AcmeCA.runner(*self.config["ca"].values())
         self.runner = runner
         self.ca = ca
 
@@ -104,3 +103,85 @@ class TestAcmetiny(TestClient, unittest.IsolatedAsyncioTestCase):
             f"--directory-url {self.DIRECTORY} --disable-check --contact {self.contact} --account-key "
             f"{account_key_path} --csr {csr_path} --acme-dir {self.data.path}/challenge "
         )
+
+
+class TestCertBot(TestClient, unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        TestClient.setUp(self)
+        with open(self.data.path / "certbot.ini", "wt") as f:
+            f.write(
+                f"""server = {self.DIRECTORY}
+config-dir = ./{self.data.path}/etc/letsencrypt
+work-dir = /home/noah/workspace/acme-broker/certbot/
+logs-dir = /home/noah/workspace/acme-broker/certbot/logs
+"""
+            )
+        self._rmtree.extend(["archive", "renewal", "live"])
+
+    async def _run(self, cmd):
+        argv = shlex.split(f"--non-interactive -c {self.data.path}/certbot.ini " + cmd)
+        import certbot._internal.main as cbm
+
+        r = await self.loop.run_in_executor(None, cbm.main, argv)
+        return r
+
+    async def test_run(self):
+        import certbot.util
+        import certbot._internal.log
+        import certbot._internal.error_handler
+
+        certbot.util.atexit_register = lambda func, *argv, **kwargs: log.info(
+            f"patched certbot.util.atexit_register for {func}"
+        )
+        certbot._internal.error_handler.ErrorHandler._set_signal_handlers = lambda x: log.info(
+            "patched certbot._internal.error_handler.ErrorHandler._set_signal_handlers"
+        )
+        certbot._internal.log.pre_arg_parse_setup = lambda: log.info(
+            "patched certbot._internal.log.pre_arg_parse_setup"
+        )
+        certbot._internal.log.post_arg_parse_setup = lambda x: log.info(
+            "patched certbot._internal.log.post_arg_parse_setup"
+        )
+
+        logging.config.dictConfig(self.config["logging"])
+
+        await self._run("certificates")
+
+        await self._run(f"register --agree-tos  -m {self.contact}")
+        domains = sorted(
+            map(lambda x: x.lower(), acme_broker.util.names_of(self.data.csr)),
+            key=lambda s: s[::-1],
+        )
+        arg = " --domain ".join(domains)
+        await self._run(
+            f"certonly --webroot --webroot-path {self.data.path} --domain {arg}"
+        )
+        arg = " --domain ".join(map(lambda s: f"dns.{s}", domains))
+        await self._run(
+            f"certonly --manual --manual-public-ip-logging-ok --preferred-challenges=dns --manual-auth-hook "
+            f'"echo $CERTBOT_VALIDATION" --manual-cleanup-hook /bin/true --domain {arg}'
+        )
+        arg = " --domain ".join(map(lambda s: f"http.{s}", domains))
+        await self._run(
+            f"certonly --manual --manual-public-ip-logging-ok --preferred-challenges=http --manual-auth-hook "
+            f'"echo $CERTBOT_VALIDATION" --manual-cleanup-hook /bin/true --domain {arg}'
+        )
+        for j in ["", "dns.", "http."]:
+            try:
+                await self._run(
+                    f"revoke --cert-path {self.data.path}/etc/letsencrypt/live/{j}{domains[0]}/cert.pem"
+                )
+            except Exception as e:
+                log.error(e)
+        await self._run("renew")
+
+    async def test_Register(self):
+        await self._run(f"register --agree-tos  -m {self.contact}")
+
+    async def test_Unregister(self):
+        try:
+            await self._run("unregister --agree-tos")
+        except Exception:
+            pass
+        await self.test_Register()
+        await self._run("unregister --agree-tos")
