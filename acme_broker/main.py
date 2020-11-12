a4828911bf1aaf2011929b5f1a52ff1ba33a9c1e
Noah Woehler <noah.woehler@gmail.com>
2020-11-12 16:51:25 +0100

    Fix main.py entrypoint. Add generate-keys command.

--- acme_broker/main.py
+++ acme_broker/main.py
@@ -1,22 +1,23 @@
 import asyncio
 import logging
 import logging.config
-import os
+from pathlib import Path
 
 import click
 import yaml
 
+from acme_broker import AcmeBroker
+from acme_broker.client import AcmeClient
+from acme_broker.client.client import DummySolver, ChallengeSolverType
 from acme_broker.server import AcmeCA
+from acme_broker.util import generate_root_cert, generate_rsa_key
 
 logger = logging.getLogger(__name__)
 
 
 def load_config(config_file):
     with open(config_file, "r") as stream:
-        try:
-            config = yaml.safe_load(stream)
-        except yaml.YAMLError as e:
-            print(e)
+        config = yaml.safe_load(stream)
 
     logging.config.dictConfig(config["logging"])
     return config
@@ -29,25 +30,66 @@ def main(ctx):
 
 
 @main.command()
-@click.option("--debug/--no-debug", default=False)
+@click.argument("root-key-file", type=click.Path())
+@click.argument("account-key-file", type=click.Path())
+def generate_keys(root_key_file, account_key_file):
+    """Generates a self-signed root key pair/cert for the CA
+    and an account key pair for the broker client"""
+    click.echo("Generating root key pair/cert")
+    generate_root_cert(
+        Path(root_key_file),
+        "DE",
+        "Lower Saxony",
+        "Hanover",
+        "ACME Broker",
+        "ACMEBroker",
+    )
+
+    click.echo("Generating broker client key pair")
+    generate_rsa_key(account_key_file)
+
+
+@main.command()
 @click.argument("config-file", type=click.Path())
-def ca(debug, config_file):
+def ca(config_file):
     """Starts the ACME CA"""
     config = load_config(config_file)
     click.echo(f'Starting ACME CA on port {config["ca"]["port"]}')
 
-    if not debug:
+    async def serve_forever():
+        await AcmeCA.runner(config["ca"])
 
-        async def serve_forever():
-            _ = await AcmeCA.runner(config["ca"])
+        while True:
+            await asyncio.sleep(3600)
 
-            while True:
-                await asyncio.sleep(3600)
+    asyncio.run(serve_forever())
 
-        asyncio.run(serve_forever())
-    else:
-        # TODO: fix or remove
-        os.system(r"adev runserver server/server.py")
+
+@main.command()
+@click.argument("config-file", type=click.Path())
+def broker(config_file):
+    """Starts the ACME Broker"""
+    config = load_config(config_file)
+    click.echo(f'Starting ACME Broker on port {config["broker"]["port"]}')
+
+    async def serve_forever():
+        broker_client = AcmeClient(
+            directory_url=config["broker"]["client"]["directory"],
+            private_key=config["broker"]["client"]["private_key"],
+            contact=config["broker"]["client"]["contact"],
+        )
+
+        broker_client.register_challenge_solver(
+            (ChallengeSolverType.DNS_01,),
+            DummySolver(),
+        )
+
+        await AcmeBroker.runner(config["broker"], client=broker_client)
+
+        while True:
+            await asyncio.sleep(3600)
+
+    asyncio.run(serve_forever())
 
 
 if __name__ == "__main__":
