6611cd108f5fdebf44166bf27b12302d854609d2
Noah Woehler <noah.woehler@gmail.com>
2020-11-21 21:34:38 +0100

    Tests: change InfobloxClient test to use _query_until_completed

--- tests/test_infoblox.py
+++ tests/test_infoblox.py
@@ -3,7 +3,6 @@ import logging.config
 import unittest
 import uuid
 
-import dns.resolver
 
 from acme_broker.client import InfobloxClient
 from acme_broker.main import load_config
@@ -25,27 +24,15 @@ class TestInfobloxClient(unittest.IsolatedAsyncioTestCase):
         self.infoblox_client = InfobloxClient(**self.config["infoblox"])
         await self.infoblox_client.connect()
 
-    async def _query_txt_record(self, name):
-        resp = await asyncio.get_event_loop().run_in_executor(
-            None, dns.resolver.resolve, name, "TXT"
-        )
-        log.info(resp.response.answer)
-        txt_record = list(resp.rrset.items.items())[0][0]
-        return txt_record.strings[0].decode()
-
     async def test_set_txt_record(self):
         test_name = self.config["infoblox_test"]["name"]
         text_value = uuid.uuid4().hex
         await self.infoblox_client.set_txt_record(test_name, text_value)
 
-        # with contextlib.suppress(dns.resolver.NXDOMAIN):
-
-        tries = 10
-        while tries > 0:
-            if await self._query_txt_record(test_name) == text_value:
-                break
-
-            tries -= 1
-            await asyncio.sleep(5)
-        else:
-            self.assertEqual(await self._query_txt_record(test_name), text_value)
+        # Poll the DNS until the correct record is available
+        try:
+            await asyncio.wait_for(
+                self.infoblox_client._query_until_completed(test_name, text_value), 60.0
+            )
+        except asyncio.TimeoutError:
+            self.fail("Could not verify that the TXT record was set")
