c149ef0a7c6ec68f97e2e9cea795b5194c4b8a63
Noah Woehler <noah.woehler@gmail.com>
2020-12-08 18:09:51 +0100

    Docker deployment: fix issue where resty-auto-ssl did not have perms for mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# Need to chown here again because the outside directory is always owned by root
+chown www-data: /etc/resty-auto-ssl
 supervisord -n -c /etc/supervisor/supervisord.conf
