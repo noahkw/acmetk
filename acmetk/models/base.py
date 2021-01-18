f74ff94c6c8666330ce0615347fe0e2cdfd9f3f3
Markus Koetter <koetter@luis.uni-hannover.de>
2021-01-18 07:33:46 +0100

    alembic - naming constraints

--- acmetk/models/base.py
+++ acmetk/models/base.py
@@ -10,11 +10,26 @@ from sqlalchemy import (
     DateTime,
     JSON,
     TypeDecorator,
+    MetaData,
 )
 from sqlalchemy.dialects.postgresql import INET
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import relationship
+
+"alembic - The Importance of Naming Constraints"
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+Base = declarative_base(metadata=meta)
+
 
 Base = declarative_base()
 
